5c280d2d762c1fee9b3507654d4f7605465a552e
Noemi Radvan <radvanno@gmail.com>
2021-11-30 21:35:37 +0100

    user model with profilepic

--- models/User.model.js
+++ models/User.model.js
@@ -8,6 +8,11 @@ const userSchema = new Schema(
       // unique: true -> Ideally, should be unique, but its up to you
     },
     password: String,
+    profilePic: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2021&q=80",
+    },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
